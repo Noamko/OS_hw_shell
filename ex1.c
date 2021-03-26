896929231fed3d0ed6c0eb050ada0400b6fafa24
Noamko <Noamko25@gmail.com>
2021-03-26 18:18:56 +0000

    added cd command

--- ex1.c
+++ ex1.c
@@ -35,17 +35,23 @@ Command* parse_command(char in[]){
 	strcpy(c->status,"RUNNING");
 	return c;
 }
+
+
 void user_input_loop() {
 	int history_counter = 0;
 	Command* history[HISTORY_LEN];
 
 	char working_directory[100];
+	char prev_working_dir[100];
+	strcpy(prev_working_dir,working_directory);
+
 	getcwd(working_directory,sizeof(working_directory));
 
 	while(1){
 
 		printf("%s", PROMPT);
-
+		fflush(stdout);
+		
 		char input[10];
 		fgets(input, COM_LEN_LIM, stdin);
 		if ((strlen(input) > 0) && (input[strlen(input) - 1] == '\n')) {
@@ -55,15 +61,55 @@ void user_input_loop() {
 		history[history_counter++] = c;
 
 		//BUILT IN COMMANDS
-
 		if(!strcmp(c->args[0],"history")) {
 			for(int i = 0; i < history_counter; i++) {
 				printf("%s",history[i]->args[0]);
+				fflush(stdout);
 				printf("	%s\n", history[i]->status);
 			}
 		}
 
-		// if (!strcmp(c->name, "cd"))
+		if(!strcmp(c->args[0], "cd")){
+			if(c->argc != 2) {
+				printf("chdir failed\n");
+			}
+			else if(!strcmp(c->args[1],"..")) {
+				for(int i = strlen(working_directory) -1; i > 0 != '/'; i--) {
+					if(working_directory[i] == '/'){
+						strcpy(prev_working_dir, working_directory);
+						working_directory[i] = '\0';
+						chdir(working_directory);
+						break;
+					}
+				}
+			}
+			else if (!strcmp(c->args[1], "~")) {
+				if(chdir(getenv("HOME")) == -1) {
+					printf("chdir failed\n");
+				}
+				strcpy(prev_working_dir, working_directory);
+				getcwd(working_directory, sizeof(working_directory));
+			}
+			else if(!strcmp(c->args[1],"-")) {
+				if(chdir(prev_working_dir) == -1) {
+					printf("chdir failed\n");
+				}
+				strcpy(prev_working_dir, working_directory);
+				getcwd(working_directory, sizeof(working_directory));
+			}
+
+			else {
+				if(chdir(c->args[1]) == -1) {
+					printf("chdir failed\n");
+				}
+				strcpy(prev_working_dir, working_directory);
+				getcwd(working_directory, sizeof(working_directory));
+			}
+		}
+
+		else if (!strcmp(c->args[0], "exit")) {
+			exit(0);
+		}
 
 
 		// if (c->name[strlen(c->name) -1] == '&' && c->name[strlen(c->name) -2] == ' '){
