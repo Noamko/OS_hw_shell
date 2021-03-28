bdf49323ac70910828fd390cd743190a17035df0
Noamko <Noamko25@gmail.com>
2021-03-28 08:14:35 +0000

    fixed running process issue

--- ex1.c
+++ ex1.c
@@ -9,73 +9,101 @@
 
 #define PROMPT "$ "
 #define HISTORY_LEN 100
-#define COM_LEN_LIM 20
+#define MAX_COMMAND_LENGTH 100
 
-typedef struct Command{
-	char args[10][10];
-	char status[8];
+typedef struct Command {
+	char com[100];
+	int* status;
+	int background;
 	int argc;
+	pid_t pid;
 }Command;
 
 
-Command* parse_command(char in[]){
-	Command* c = malloc(sizeof(Command));
-	int argc = 0;
-	int len = strlen(in);
-	for (int i = 0; i < len; i++) {
-		if (in[i] == ' ') {
-			argc++;
-		}
-		else {
-			char k = in[i];
-			strncat(c->args[argc], &k, 1);
-		}
+void runInBackground(pid_t pid, char* args[]) {
+	if (pid == 0) {
+		execvp(args[0], args);
 	}
-	c->argc = argc + 1;
-	strcpy(c->status,"RUNNING");
-	return c;
 }
 
+void run(pid_t pid, char* args[]) {
+	if (pid == 0) {
+		execvp(args[0], args);
+	}
+	else {
+		wait(NULL);
+	}
+}
 
 void user_input_loop() {
 	int history_counter = 0;
-	Command* history[HISTORY_LEN];
+	Command history[HISTORY_LEN];
 
 	char working_directory[100];
 	char prev_working_dir[100];
-	strcpy(prev_working_dir,working_directory);
+	strcpy(prev_working_dir, working_directory);
+	getcwd(working_directory, sizeof(working_directory));
 
-	getcwd(working_directory,sizeof(working_directory));
-
-	while(1){
-
+	while (1) {
 		printf("%s", PROMPT);
 		fflush(stdout);
-		
-		char input[10];
-		fgets(input, COM_LEN_LIM, stdin);
+
+		char input[MAX_COMMAND_LENGTH];
+		// scanf("%s",input);
+		fgets(input, MAX_COMMAND_LENGTH, stdin);
 		if ((strlen(input) > 0) && (input[strlen(input) - 1] == '\n')) {
 			input[strlen(input) - 1] = '\0';
 		}
-		Command* c = parse_command(input);
-		history[history_counter++] = c;
+
+		Command c;
+		c.background = 0;
+		int argc = 0;
+		char* token = strtok(input, " ");
+		char* args[100];
+		args[0] = token;
+		strcpy(c.com, input);
+		while (token != NULL) {
+			argc++;
+			token = strtok(NULL, " ");
+			args[argc] = token;
+			if(token != NULL){
+				strcat(c.com, " ");
+				strcat(c.com, token);
+			}
+		}
+		c.argc = argc;
+		if(!strcmp(args[argc -1], "&")) {
+			c.background = 1;
+			c.com[strlen(c.com) - 2] = '\0';
+		}
+		history[history_counter] = c;
+		history_counter++;
 
 		//BUILT IN COMMANDS
-		if(!strcmp(c->args[0],"history")) {
-			for(int i = 0; i < history_counter; i++) {
-				printf("%s",history[i]->args[0]);
+		if (!strcmp(c.com, "history")) {
+			for (int i = 0; i < history_counter; i++) {
+
+				// waitpid(history[i].pid,NULL,WNOHANG);
+				// int stat;
+				// printf("%s : %d : %d\n", history[i].com, history[i].pid, waitpid(history[i].pid,NULL,WNOHANG));
+				printf("%s", history[i].com);
 				fflush(stdout);
-				printf("	%s\n", history[i]->status);
+				if (waitpid(history[i].pid, NULL, WNOHANG) == 0 || i == history_counter-1) {
+					printf(" RUNNING\n");
+				}
+				else if (waitpid(history[i].pid, NULL, WNOHANG) == -1) {
+					printf(" DONE\n");
+				}
 			}
 		}
 
-		if(!strcmp(c->args[0], "cd")){
-			if(c->argc != 2) {
-				printf("chdir failed\n");
+		else if (!strcmp(c.com, "cd")) {
+			if (c.argc > 2) {
+				printf("Too many argument\n");
 			}
-			else if(!strcmp(c->args[1],"..")) {
-				for(int i = strlen(working_directory) -1; i > 0 != '/'; i--) {
-					if(working_directory[i] == '/'){
+			else if (!strcmp(args[1], "..")) {
+				for (int i = strlen(working_directory); i > 0; i--) {
+					if (working_directory[i] == '/') {
 						strcpy(prev_working_dir, working_directory);
 						working_directory[i] = '\0';
 						chdir(working_directory);
@@ -83,15 +111,15 @@ void user_input_loop() {
 					}
 				}
 			}
-			else if (!strcmp(c->args[1], "~")) {
-				if(chdir(getenv("HOME")) == -1) {
+			else if (!strcmp(args[1], "~")) {
+				if (chdir(getenv("HOME")) == -1) {
 					printf("chdir failed\n");
 				}
 				strcpy(prev_working_dir, working_directory);
 				getcwd(working_directory, sizeof(working_directory));
 			}
-			else if(!strcmp(c->args[1],"-")) {
-				if(chdir(prev_working_dir) == -1) {
+			else if (!strcmp(args[1], "-")) {
+				if (chdir(prev_working_dir) == -1) {
 					printf("chdir failed\n");
 				}
 				strcpy(prev_working_dir, working_directory);
@@ -99,7 +127,7 @@ void user_input_loop() {
 			}
 
 			else {
-				if(chdir(c->args[1]) == -1) {
+				if (chdir(args[1]) == -1) {
 					printf("chdir failed\n");
 				}
 				strcpy(prev_working_dir, working_directory);
@@ -107,31 +135,40 @@ void user_input_loop() {
 			}
 		}
 
-		else if (!strcmp(c->args[0], "exit")) {
+		else if (!strcmp(c.com, "jobs")) {
+			for (int i = 0; i < history_counter; i++)
+			{
+				if (*history[i].status == 1 && history[i].background) {
+					printf("%s\n", history[i].com);;
+				}
+			}
+		}
+		else if (!strcmp(c.com, "exit")) {
 			exit(0);
 		}
 
-
-		// if (c->name[strlen(c->name) -1] == '&' && c->name[strlen(c->name) -2] == ' '){
-		// 	pid_t p = fork();
-		// 	if(p == 0) {
-		// 		execlp(c->name,c->name, NULL);
-		// 	}
-		// }
-		// pid_t p = fork();
-		// if(p == 0) {
-		// 	execlp(c.name,c.name, NULL);
-		// 	exit(0);
-		// }
-		// else{
-		// 	wait(&c.status);
-		// }
-		strcpy(c->status, "DONE");
+		//NON Built in COMs
+		else {
+			c.pid = fork();
+			if(c.pid != 0) {
+				history[history_counter-1].pid = c.pid;
+			}
+			if (c.background) {
+				args[c.argc - 1] = '\0';
+				runInBackground(c.pid, args);
+			}
+			else {
+				run(c.pid, args);
+			}
+		}
+	}
+	
+	for (int i = 0; i < history_counter; i++) {
+		free(history[i].status);
 	}
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	user_input_loop();
 	return 0;
 }
