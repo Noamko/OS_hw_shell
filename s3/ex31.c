fa02950812e550259013185d852061484a8dd9d3
Noam Koren <noamko25@gmail.com>
2021-05-14 17:51:27 +0300

    almost done need some refactoring

--- s3/ex31.c
+++ s3/ex31.c
@@ -6,40 +6,40 @@
 #include <sys/types.h>
 #include <sys/fcntl.h>
 
-int check_equal(int f1, int f2, int _size){
+int check_equal(int f1, int f2, int _size) {
 	char f1_buffer[_size + 1];
 	char f2_buffer[_size + 1];
 	int bytes_read = _size;
 	do {
-		bytes_read = read(f1,f1_buffer, _size);
-		int t = read(f2,f2_buffer,_size);
-		if( bytes_read == -1 || t == -1){
-			fprintf(stderr, "Error in: %s\n", __func__); 
+		bytes_read = read(f1, f1_buffer, _size);
+		int t = read(f2, f2_buffer, _size);
+		if (bytes_read == -1 || t == -1) {
+			fprintf(stderr, "Error in: %s\n", __func__);
 			return -1;
 		}
-		if(t != bytes_read){
+		if (t != bytes_read) {
 			return 0;
 		}
-		for(int i = 0; i < bytes_read; i++) {
-			if(f1_buffer[i] != f2_buffer[i]) {
+		for (int i = 0; i < bytes_read; i++) {
+			if (f1_buffer[i] != f2_buffer[i]) {
 				return 0;
 			}
 		}
-	} while(bytes_read == _size);
+	} while (bytes_read == _size);
 	return 1;
 }
 char get_next_nonbreak_char(int fd) {
 	char temp_buffer[2];
 	do {
-		int r = read(fd,temp_buffer,1);
-		if(r == -1){
-			fprintf(stderr, "Error in: %s\n", __func__); 
+		int r = read(fd, temp_buffer, 1);
+		if (r == -1) {
+			fprintf(stderr, "Error in: %s\n", __func__);
 			return 0;
 		}
-		if(r == 0){
+		if (r == 0) {
 			return EOF;
 		}
-	} while (temp_buffer[0] == '\n' || temp_buffer[0] == '\t' || temp_buffer[0] == 32);
+	} while (temp_buffer[0] == '\n' || temp_buffer[0] == '\t' || temp_buffer[0] == 32 || temp_buffer[0] == '\r');
 	return temp_buffer[0];
 }
 int check_similar(int fd1, int fd2, int _size) {
@@ -47,10 +47,10 @@ int check_similar(int fd1, int fd2, int _size) {
 	do {
 		c1 = get_next_nonbreak_char(fd1);
 		c2 = get_next_nonbreak_char(fd2);
-		if(c1 == 0 || c2 == 0){
+		if (c1 == 0 || c2 == 0) {
 			return -1;
 		}
-		if(c1 != c2){
+		if (c1 != c2) {
 			return 2;
 		}
 
@@ -58,30 +58,30 @@ int check_similar(int fd1, int fd2, int _size) {
 	return 3;
 }
 
-int main(int argc, char* argv[]){
-	int file1,file2;
+int main(int argc, char* argv[]) {
+	int file1, file2;
 	file1 = open(argv[1], O_RDONLY);
 	file2 = open(argv[2], O_RDONLY);
-	if(file1 < 0 || file2 < 0) {
-		fprintf(stderr, "Error in: %s\n", __func__); 
+	if (file1 < 0 || file2 < 0) {
+		fprintf(stderr, "Error in: %s\n", __func__);
 		close(file1);
 		close(file2);
 		return -1;
 	}
-	int equal = check_equal(file1,file2,100);
-	if(equal == -1 ) {
+	int equal = check_equal(file1, file2, 100);
+	if (equal == -1) {
 		close(file1);
 		close(file2);
 		return -1;
 	}
-	else if(equal){
+	else if (equal) {
 		close(file1);
 		close(file2);
 		return 1;
 	}
-	lseek(file1,0,SEEK_SET);
-	lseek(file2,0,SEEK_SET);
-	int similar = check_similar(file1,file2,100);
+	lseek(file1, 0, SEEK_SET);
+	lseek(file2, 0, SEEK_SET);
+	int similar = check_similar(file1, file2, 100);
 	close(file1);
 	close(file2);
 	return similar;
--- s3/ex32.c
+++ s3/ex32.c
@@ -6,37 +6,221 @@
 #include <sys/types.h>
 #include <sys/fcntl.h>
 #include <string.h>
+#include <time.h>
 
-char* run(const char* exe) {
-	return "hello from run";
+#define INPUT_SIZE 2048
+typedef struct student_report {
+	int score;
+	const char* name;
+	const char* r_info;
+	struct student_report* next;
+}student_report;
+
+student_report* add(student_report* sr, const char* name, const char* info, int score) {
+	if (sr == NULL) {
+		sr = malloc(sizeof(student_report));
+		sr->score = score;
+		sr->name = name;
+		sr->r_info = info;
+	}
+	else {
+		student_report* temp = sr;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = malloc(sizeof(student_report));
+		temp->next->score = score;
+		temp->next->name = name;
+		temp->next->r_info = info;
+	}
+	return sr;
+}
+int isDirectory(const char* path) {
+	struct stat statbuf;
+	if (stat(path, &statbuf) != 0)
+		return 0;
+	return S_ISDIR(statbuf.st_mode);
 }
 
-char* enter_and_run(const char* folder) {
-	// look for a single .c file 
-	// run it 
-	// return output using pipe and dupe2 or dupe
-	return "none";
+int compareFiles(const char* comp, const char* file1, const char* file2) {
+	pid_t pid = fork();
+	if (pid == 0) {
+		if (execl(comp, comp, file1, file2, (char*)NULL) < 0) {
+			printf("failed to exec");
+			return -1;
+		}
+	}
+	else {
+		int status;
+		wait(&status);
+		return WEXITSTATUS(status);
+	}
+	return -1;
 }
-int main(int argc, char* argv[]) {
 
-	int info_file = open(argv[1], O_RDONLY);
-	int size = 2024;
-	char buffer[size + 1];
-	int bytes_read = size;
-	do {
-		bytes_read = read(info_file, buffer, size);
+void set_output(const char* file) {
+	int out = open(file, O_WRONLY | O_CREAT, 0666);
+	dup2(out, STDOUT_FILENO);
+	close(out);
+}
 
-	} while (bytes_read == size);
-	char* _dir = strtok(buffer, "\n");
-	char* _input = strtok(NULL, "\n");
-	char* _correct_output = strtok(NULL, "\n");
+void set_input(const char* file) {
+	int in = open(file, O_RDONLY);
+	if (dup2(in, STDIN_FILENO) < 0) {
+		printf("unable to dup stdin\n");
+	}
+	close(in);
+}
+
+void set_error(const char* file) {
+	int err = open(file, O_WRONLY | O_CREAT, 0666);
+	if (err < 0) {
+		printf("failed to create errors.txt");
+		exit(1);
+	}
+	if (dup2(err, STDERR_FILENO) < 0) {
+		printf("failed to dup err");
+	}
+	close(err);
+}
+
+int isCFile(const char* file) {
+	int len = strlen(file);
+	if (len >= 3) {
+		if (file[len - 2] == '.' && file[len - 1] == 'c') {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+const char* getFullPath(const char* file) {
+	char* str = malloc(255);
+	char path[255];
+	getcwd(path, sizeof(path));
+	strcat(path, "/");
+	strcat(path, file);
+	strcpy(str, path);
+	return str;
+}
+
+student_report enter_and_run(const char* folder, const char* inputfile, const char* currect_output, const char* comp) {
+	DIR* dir = opendir(folder);
+	struct dirent* dn;
+	chdir(folder);
+	student_report report;
+	while ((dn = readdir(dir)) != NULL) {
+		char* file = dn->d_name;
+		if (isCFile(file)) { // c file found.
+			time_t begin, end;
+			time(&begin);
+			pid_t pid = fork();
+			if (pid == 0) {
+				set_error("errors.txt");
+				pid_t pid2 = fork();
+				if (pid2 == 0) {
+					if (execl("/usr/bin/gcc", "gcc", file, (char*)NULL) < 0) {
+						exit(2);
+					}
+				}
+				else {
+					wait(NULL);
+					set_input(inputfile);
+					set_output("output.txt");
+					if (execl("./a.out", (char*)NULL) < 0) {
+						exit(2);
+					}
+				}
+			}
+			int status;
+			wait(&status);
+			time(&end);
+			time_t elapsed = end - begin;
+			int rs = WEXITSTATUS(status);
+			int cp = compareFiles(comp, getFullPath("output.txt"), currect_output);
+			remove("output.txt");
+			remove("a.out");
+			remove("errors.txt");
+			report.name = folder;
+			if (elapsed > 4) {
+				report.score = 20;
+				report.r_info = "TIMEOUT";
+			}
+			else if (rs == 2) {
+				report.score = 10;
+				report.r_info = "COMPILATION_ERROR";
+			}
+			else if (cp == 1) {
+				report.score = 100;
+				report.r_info = "EXCELLENT";
+			}
+			else if (cp == 2) {
+				report.score = 50;
+				report.r_info = "WRONG";
+			}
+			else if (cp == 3) {
+				report.score = 75;
+				report.r_info = "SIMILAR";
+			}
+			closedir(dir);
+			chdir("..");
+			return report;
+		}
+	}
+	//fail no c file
+	report.score = 0;
+	report.name = folder;
+	report.r_info = "NO_C_FILE";
+	closedir(dir);
+	chdir("..");
+	return report;
+}
+
+student_report* ex32(const char* path) {
+	int fd = open(path, O_RDONLY);
+	char buffer[INPUT_SIZE + 1];
+	int bytes_read = INPUT_SIZE;
+	bytes_read = read(fd, buffer, INPUT_SIZE); //taking a risk here that file content is larger then 2048 (not likly at all)
+	close(fd);
+
+	const char* _dir = strtok(buffer, "\n");
+	const char* _input_file = getFullPath(strtok(NULL, "\n"));
+	const char* _correct_output_file = getFullPath(strtok(NULL, "\n"));
+	const char* comp = getFullPath("comp.out");
 
 	DIR* dir = opendir(_dir);
 	struct dirent* dn;
+	chdir(_dir);
+
+	student_report* list;
 	while ((dn = readdir(dir)) != NULL) {
-		char* f = dn->d_name;
-		struct stat path_stat;
-		stat(f, &path_stat);
-		printf("%s : %d\n", f, S_ISDIR(path_stat.st_mode));
+		char* folder = dn->d_name;
+		if (isDirectory(folder) && strcmp(folder, "..") != 0 && strcmp(folder, ".") != 0) {
+			student_report _report = enter_and_run(folder, _input_file, _correct_output_file, comp); // look for a .c file compile and run it.
+			list = add(list, _report.name, _report.r_info, _report.score);
+		}
 	}
+	closedir(dir);
+	return list;
+}
+
+void create_report(student_report* r) {
+	student_report* temp = r;
+	FILE* fpt = fopen("results1.csv", "w+");
+	char str[255];
+	char score_str[3];
+	while (r != NULL) {
+		fprintf(fpt, "%s,%d,%s\n", r->name, r->score, r->r_info);
+		r = r->next;
+	}
+	fclose(fpt);
+}
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		printf("not enough args");
+		exit(1);
+	}
+	student_report* lst = ex32(argv[1]);
+	create_report(lst);
+	return 0;
 }
